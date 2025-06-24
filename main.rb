ff15921d0e09f477304f750671d61bbc57c1c17d
Faris Iskandar Hafidz <151354421+Hafezaul@users.noreply.github.com>
2025-06-24 08:02:57 +0000

    feat : pratice at ruby

---
+++ main.rb
@@ -0,0 +1,12 @@
+nama_orang = [
+  "Faris", "Alya", "Raka", "Zahra", "Dimas", "Nina", "Bagas", "Citra", "Ardi", "Maya",
+  "Rio", "Lia", "Hadi", "Putri", "Bima", "Salma", "Yusuf", "Nadya", "Iqbal", "Intan",
+  "Galih", "Sari", "Reza", "Bella", "Tegar", "Winda", "Rizky", "Aurel", "Andre", "Rani",
+  "Ilham", "Nabila", "Adit", "Karina", "Dion", "Nesa", "Rico", "Tasya", "Fauzan", "Gita",
+  "Vino", "Syifa", "Dhani", "Laras", "Eka", "Ayu", "Hana", "Zaki", "Nino", "Tiara",
+  "Faris", "Alya", "Rani", "Iqbal", "Nina", "Raka", "Zahra", "Yusuf", "Bagas"
+]
+
+
+urut_nama = nama_orang.uniq!.sort
+p urut_nama
